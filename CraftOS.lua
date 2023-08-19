375f3c38c1c0b4646b4cb6b475cd16f4c37e9bf1
badgeminer <64038298+badgeminer@users.noreply.github.com>
2023-08-18 19:37:49 -0600

    Update CraftOS.lua

--- CraftOS.lua
+++ CraftOS.lua
@@ -1,4 +1,5 @@
 return {
   class = {pt="file",host="github",loc="CCPackages/collections/%s/class.lua",filename="class.lua"},
+  dim = {pt="file",host="github",loc="CCPackages/collections/%s/dim.lua",filename="dim.lua"},
   ui = {pt="dim",host="github",loc="CCPackages/collections/%s/ui.dim"}
 }
