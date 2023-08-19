d30f109a8e5fb9eebb788d8e77cc736cc94e33f9
badgeminer <64038298+badgeminer@users.noreply.github.com>
2023-08-18 19:30:48 -0600

    add ui package

--- CraftOS.lua
+++ CraftOS.lua
@@ -1,3 +1,4 @@
 return {
   class = {pt="file",host="github",loc="CCPackages/collections/%s/class.lua",filename="class.lua"},
+  ui = {pt="dim",host="github",loc="CCPackages/collections/%s/ui.dim"}
 }
